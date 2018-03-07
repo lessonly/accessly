ca37a1b0ea5647208ccc89da2ca30d99456a61ca
Aaron Milam <aaron@lesson.ly>
2018-03-07 11:19:50 -0500

    Added API

--- lib/accesscontrol.rb
+++ lib/accesscontrol.rb
@@ -1,5 +1,16 @@
 require "accesscontrol/version"
 
-module Accesscontrol
-  # Your code goes here...
+module AccessControl
+
+  def can?(actor, action_id, object_name, object_id = nil)
+  end
+
+  def list(actor, action_id, object_name)
+  end
+
+  def grant(actor, action_id, object_name, object_id = nil)
+  end
+
+  def revoke(actor, action_id, object_name, object_id = nil)
+  end
 end
