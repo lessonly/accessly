a19511b6a4203ad9d5f7cb52e2f6fcbe307fa70c
Aaron Milam <aaron@lesson.ly>
2018-03-12 12:59:59 -0400

    Cache permitted action on object lookups
    AccessControl::PermittedActionOnObjectQuery#can? will now cache the results of a lookup so that it doesn't call to the database again when the same call is made.

--- lib/accesscontrol/permitted_action_on_object_query.rb
+++ lib/accesscontrol/permitted_action_on_object_query.rb
@@ -19,15 +19,41 @@ module AccessControl
     #   # Can the user perform the action with id 5 for the Post with id 7?
     #   AccessControl::Records.can?(user, 5, Post, 7)
     def can?(action_id, object_type, object_id)
-      PermittedActionOnObject.where(
-        actor: @actor,
-        action: action_id,
-        object_type: String(object_type),
-        object_id: object_id
-      ).exists?
+      find_or_set_value(:can, @actor.class.name, @actor.id, action_id, object_type) do
+        PermittedActionOnObject.where(
+          actor: @actor,
+          action: action_id,
+          object_type: String(object_type),
+          object_id: object_id
+        ).exists?
+      end
     end
 
     def list(action_id, object_type)
+    end
+
+    private
+
+    def past_lookups
+      @_past_lookups ||= {}
+    end
+
+    def find_or_set_value(*keys, &query)
+      found_value = past_lookups.dig(*keys)
+      found_value = found_value.nil? ? query.call : found_value
+      set_value(*keys, value: found_value)
+
+      found_value
+    end
+
+    def set_value(*keys, value:)
+      lookup = past_lookups
+      keys[0..-2].each do |key|
+        lookup[key] ||= {}
+        lookup = lookup[key]
+      end
+
+      lookup[keys[-1]] = value
     end
   end
 end
--- test/cached_results_test.rb
+++ test/cached_results_test.rb
@@ -16,4 +16,25 @@ describe AccessControl do
     query.can?(1, Post).must_equal false
     AccessControl::Query.new(actor).can?(1, Post).must_equal true
   end
+
+  it 'caches the result of permitted action on object' do
+    actor = User.create!
+    post = Post.create!
+
+    permitted_action = AccessControl::PermittedActionOnObject.create!(
+      id: SecureRandom.uuid,
+      actor: actor,
+      action: 9,
+      object_type: Post,
+      object_id: post.id
+    )
+
+    query = AccessControl::Query.new(actor)
+    query.can?(9, Post, post.id).must_equal true
+
+    permitted_action.destroy!
+
+    query.can?(9, Post, post.id).must_equal true
+    AccessControl::Query.new(actor).can?(9, Post, post.id).must_equal false
+  end
 end
