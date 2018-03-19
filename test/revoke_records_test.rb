6eef979cfa4096f4de00f0daecd00b530efab093
ehourigan <ehouriga@gmail.com>
2018-03-19 18:08:42 -0400

    add segment additions to revoke

--- test/revoke_records_test.rb
+++ test/revoke_records_test.rb
@@ -13,6 +13,17 @@ describe AccessControl do
     AccessControl::PermittedActionOnObject.where(actor: actor).count.must_equal 0
   end
 
+  it "returns nil after a successful revoke on a segment" do
+    actor = User.create!
+    post = Post.create!
+
+    AccessControl::PermittedActionOnObject.create!(id: SecureRandom.uuid, segment_id: 1, actor: actor, action: 1, object_type: Post, object_id: post.id)
+    AccessControl::PermittedActionOnObject.where(actor: actor, segment_id: 1).count.must_equal 1
+
+    assert_nil(AccessControl::Permission::Revoke.new(actor).on_segment(1).revoke(1, Post, post.id))
+    AccessControl::PermittedActionOnObject.where(actor: actor, segment_id: 1).count.must_equal 0
+  end
+
   it "raises an error when attempting to revoke on an actor that is not an ActiveRecord::Base object" do
     actor = User.create!
 
