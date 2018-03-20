7131315a480e90c9428a545ba644506f33c7188b
Aaron Milam <aaron@lesson.ly>
2018-03-20 15:29:02 -0400

    Add base policy
    Add method of creating actions in the policy. This uses define_method to
    create methods based on those actions. Those methods respond appropriately
    when given or not given an object based on whether the action was defined
    for OnObject contexts or otherwise.

--- lib/accessly/models/permitted_action_on_object.rb
+++ lib/accessly/models/permitted_action_on_object.rb
@@ -3,5 +3,6 @@ require "active_record"
 module Accessly
   class PermittedActionOnObject < ActiveRecord::Base
     belongs_to :actor, polymorphic: true
+    belongs_to :object, polymorphic: true
   end
 end
