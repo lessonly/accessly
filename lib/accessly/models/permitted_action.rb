9a764f869b192e1ed583774aa0a5974477f2e5b1
Caryssa Perez <caryssa@iris-works.com>
2021-02-03 09:14:43 -0500

    Add hooks

--- lib/accessly/models/permitted_action.rb
+++ lib/accessly/models/permitted_action.rb
@@ -3,5 +3,15 @@ require "active_record"
 module Accessly
   class PermittedAction < ActiveRecord::Base
     belongs_to :actor, polymorphic: true
+
+    before_create :set_uuid
+
+    private
+
+    def set_uuid
+      return unless PermittedActionOnObject.columns_hash['id'].type == :uuid
+
+      self.id = SecureRandom.uuid
+    end
   end
 end
--- lib/accessly/models/permitted_action_on_object.rb
+++ lib/accessly/models/permitted_action_on_object.rb
@@ -4,5 +4,15 @@ module Accessly
   class PermittedActionOnObject < ActiveRecord::Base
     belongs_to :actor, polymorphic: true
     belongs_to :object, polymorphic: true
+
+    before_create :set_uuid
+
+    private
+
+    def set_uuid
+      return unless PermittedActionOnObject.columns_hash['id'].type == :uuid
+
+      self.id = SecureRandom.uuid
+    end
   end
 end
